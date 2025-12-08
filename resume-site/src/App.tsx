b7ff9ae95c507baa9d7f9850a9075132fd8e8d3e
SpencerELewis <spencerlewis12304@gmail.com>
2025-12-07 21:03:40 -0600

    Removing unused components

--- resume-site/src/App.tsx
+++ resume-site/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
-import Home from './components/Home'
 import Projects from './components/Projects'
 import Resume from './components/Resume'
 import 'bootstrap/dist/css/bootstrap.min.css'
